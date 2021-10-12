b3a21e3a4de14679dd47e48b1676a76226b22b51
kritika-testaccount <92358991+kritika-testaccount@users.noreply.github.com>
2021-10-12 12:43:25 +0530

    Create AddNums.java

---
+++ AddNums.java
@@ -0,0 +1,9 @@
+public class AddNums {
+   public static void main(String[] args) {
+      System.out.println(add_Nums(1,2));
+   }
+   
+  public static int add_Nums(int a, int b) {
+  return a+b;
+  }
+}
