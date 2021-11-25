761e31ef29a9cc569442f324a7eb89f60e67cfb7
kritika-testaccount <92358991+kritika-testaccount@users.noreply.github.com>
2021-11-25 22:03:13 +0530

    Update installfiletest1.sql

--- installfiletest1.sql
+++ installfiletest1.sql
@@ -1,7 +1,4 @@
-create table t1 (c1 number, c2 number);
-insert into t1 values(1,2);
-insert into t1 values(3,4);
-insert into t1 values(5,6);
-insert into t1 values(7,8);
-insert into t1 values(9,0);
+create table t1 (c1 number, c2 number)
+/
+
 
